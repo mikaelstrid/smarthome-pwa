d002b13bf05e8b47e09416b3f5588f86e6448fb0
Mikael Strid <mikael.strid@vasttrafik.se>
2018-10-11 10:07:25 +0200

    Run "ng serve" independently.

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -61,7 +61,7 @@ namespace SmartHome.Pwa.Web
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
